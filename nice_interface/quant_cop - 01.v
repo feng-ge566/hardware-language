370173ba735ad748399be350ff7e92128d9ae472
feng-ge566 <152604866+feng-ge566@users.noreply.github.com>
2023-12-02 20:10:49 +0800

    Update quant_cop - 01.v

--- nice_interface/quant_cop - 01.v
+++ nice_interface/quant_cop - 01.v
@@ -1,27 +1,4 @@
-/*                                                                      
- Copyright 2018-2020 Nuclei System Technology, Inc.                
-                                                                         
- Licensed under the Apache License, Version 2.0 (the "License");         
- you may not use this file except in compliance with the License.        
- You may obtain a copy of the License at                                 
-                                                                         
-     http://www.apache.org/licenses/LICENSE-2.0                          
-                                                                         
-  Unless required by applicable law or agreed to in writing, software    
- distributed under the License is distributed on an "AS IS" BASIS,       
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and     
- limitations under the License.                                          
- */
 
-//=====================================================================
-//
-// Designer   : LZB
-//
-// Description:
-//  The Module to realize a simple NICE core
-//
-// ====================================================================
 `include "e203_defines.v"
 
 `ifdef E203_HAS_NICE//{
